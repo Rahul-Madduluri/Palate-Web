0e2a09e6705764bd23adad336f9869b647f7718f
rahulmadduluri <rahulmadduluri@gmail.com>
2014-01-25 07:17:52 -0800

    Finish sign in

--- palate_app/app/controllers/users_controller.rb
+++ palate_app/app/controllers/users_controller.rb
@@ -12,8 +12,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-    	flash[:success] = "Welcome to Palate!"
-      	redirect_to @user
+      sign_in @user
+      flash[:success] = "Welcome to Palate!"
+      redirect_to @user
     else
       	render 'new'
     end
