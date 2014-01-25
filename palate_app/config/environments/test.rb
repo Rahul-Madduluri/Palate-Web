880a6fcc7e124672bc2ee55b74cfb318e102a46b
Spencer Congero <congero@usc.edu>
2014-01-25 05:57:08 -0800

    Finish user signup

--- palate_app/config/environments/test.rb
+++ palate_app/config/environments/test.rb
@@ -33,4 +33,6 @@ PalateApp::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  ActiveModel::SecurePassword.min_cost = true
 end
---
+++ palate_app/spec/factories.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+	factory :user do
+		name        "Rahul Madduluri"
+		email       "rahul@madduluri.com"
+		password    "password"
+		password_confirmation "password"
+	end
+
+	
+
+end
