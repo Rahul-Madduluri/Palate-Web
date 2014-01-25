f20d7a6e9ccebf0e525f8ab072430a078e20aca6
rahulmadduluri <rahulmadduluri@gmail.com>
2014-01-25 01:52:21 -0800

    insig changes

--- palate_app/spec/requests/static_pages_spec.rb
+++ palate_app/spec/requests/static_pages_spec.rb
@@ -4,10 +4,12 @@ describe "StaticPages" do
 
   describe "Not signed in home page" do
 
-    it "should have the content 'Palate'" do
+    it "should have the title 'Palate'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
       expect(page).to have_title('Palate | Home')
     end
+
+
   end
 end
