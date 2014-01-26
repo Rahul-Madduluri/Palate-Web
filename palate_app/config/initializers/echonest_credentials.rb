849ae60ae5521d0f6844e69b53b6c0ab3ae4535c
rahulmadduluri <rahulmadduluri@gmail.com>
2014-01-25 17:24:36 -0800

    added API gems

---
+++ palate_app/config/initializers/echonest_credentials.rb
@@ -0,0 +1,7 @@
+require 'echowrap'
+
+Echowrap.configure do |config|
+  config.api_key =       'AJWC4ET3YVMEBFCUH'
+  config.consumer_key =  'd429642fc2978dcb196dae434b116703'
+  config.shared_secret = 'JC+sjGdhT5yg1Jnoo0KU/w'
+end
---
+++ palate_app/config/initializers/rottentomatoes_credentials.rb
@@ -0,0 +1,6 @@
+require 'rubygems'
+require 'rottentomatoes'
+include RottenTomatoes
+
+# setup your API key
+Rotten.api_key = "xkq5yzchy5wnpgjkdfuzb2m6"
