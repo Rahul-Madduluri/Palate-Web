a2238c1e03ca0afe9e7154f8894e88232a36a94f
rahulmadduluri <maddulur@usc.edu>
2014-03-14 00:54:17 -0700

    updating the profile page

--- palate_app/app/helpers/users_helper.rb
+++ palate_app/app/helpers/users_helper.rb
@@ -8,6 +8,35 @@ module UsersHelper
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 
+  def display_profile_bits
+ 
+
+  	if (current_user.adventurousness_affinity > 0.5)
+  		first_letter = "I"
+  	else
+  		first_letter = "C"
+  	end
+  	if (current_user.instinctiveness_affinity > 0.5)
+  		second_letter = "V"
+  	else
+  		second_letter = "Ce"
+  	end
+  	if (current_user.pace_affinity > 0.5)
+  		third_letter = "E"
+  	else
+  		third_letter = "R"
+  	end
+  	if (current_user.valence_affinity > 0.5)
+  		fourth_letter = "J"
+  	else
+  		fourth_letter = "En"
+  	end
+
+  	profile_bits = first_letter + second_letter + third_letter + fourth_letter
+  	profile_bits
+
+
+  end
  
 
 end
