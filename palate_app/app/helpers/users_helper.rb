f1958ebf16f789643e85f9acbbc1ccbd423b8942
rahulmadduluri <maddulur@usc.edu>
2014-03-14 21:27:07 -0700

    added some changes to user profile

--- palate_app/app/helpers/users_helper.rb
+++ palate_app/app/helpers/users_helper.rb
@@ -10,33 +10,34 @@ module UsersHelper
 
   def display_profile_bits
  
+  	if ( current_user == @user) 
+	  	if (@user.adventurousness_affinity > 0.5)
+	  		first_letter = "I"
+	  	else
+	  		first_letter = "C"
+	  	end
+	  	if (@user.instinctiveness_affinity > 0.5)
+	  		second_letter = "V"
+	  	else
+	  		second_letter = "Ce"
+	  	end
+	  	if (@user.pace_affinity > 0.5)
+	  		third_letter = "E"
+	  	else
+	  		third_letter = "R"
+	  	end
+	  	if (@user.valence_affinity > 0.5)
+	  		fourth_letter = "J"
+	  	else
+	  		fourth_letter = "En"
+	  	end
 
-  	if (current_user.adventurousness_affinity > 0.5)
-  		first_letter = "I"
-  	else
-  		first_letter = "C"
-  	end
-  	if (current_user.instinctiveness_affinity > 0.5)
-  		second_letter = "V"
-  	else
-  		second_letter = "Ce"
-  	end
-  	if (current_user.pace_affinity > 0.5)
-  		third_letter = "E"
-  	else
-  		third_letter = "R"
-  	end
-  	if (current_user.valence_affinity > 0.5)
-  		fourth_letter = "J"
-  	else
-  		fourth_letter = "En"
-  	end
-
-  	profile_bits = first_letter + second_letter + third_letter + fourth_letter
-  	profile_bits
+	  	profile_bits = first_letter + second_letter + third_letter + fourth_letter
+	  	profile_bits
 
 
-  end
+	  end
+	end
  
 
 end
