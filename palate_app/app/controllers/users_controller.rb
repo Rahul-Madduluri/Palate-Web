b130060681bc72818dcddccf1aaa9c9939f9db5b
rahulmadduluri <maddulur@usc.edu>
2014-03-31 23:17:22 -0700

    some more UI changes to user show page

--- palate_app/app/controllers/users_controller.rb
+++ palate_app/app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
+    @user_feed_items  = @user.microposts.take(20)
     @adventurousness
     @instinctiveness
     @pace
